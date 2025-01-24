501864f59f9300c5c401c0d1861f025d8f1a4f32
Alex Fernandes <alexfernands@outlook.com>
2025-01-23 22:11:14 -0500

    updated

--- app/layout.js
+++ app/layout.js
@@ -6,17 +6,16 @@ import { Container, ThemeProvider, CssBaseline } from "@mui/material";
 import theme from "../themes"; // Adjust the path if needed
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<body>
-				<ThemeProvider theme={theme}>
-					<CssBaseline />
-					<Container>
-						<NavBar />
-						{children} <Footer />
-					</Container>
-				</ThemeProvider>{" "}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <NavBar />
+          <Container>{children}</Container>
+          <Footer />
+        </ThemeProvider>{" "}
+      </body>
+    </html>
+  );
 }
