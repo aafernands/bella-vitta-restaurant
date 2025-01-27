0eddce1124245cf99fb29346be77ae515508781a
Alex Fernandes <62030201+aafernands@users.noreply.github.com>
2025-01-27 15:21:53 -0500

    SEO Bug Fix2

--- components/SEO.js
+++ components/SEO.js
@@ -1,19 +1,14 @@
-"use client";
+// SEO.js
+import React from 'react';
+import Head from 'next/head';
 
-import Head from "next/head";
-
-const SEO = ({ title, description, keywords, author }) => {
-	return (
-		<Head>
-			<title>{title}</title>
-			<meta name="description" content={description} />
-			{keywords && <meta name="keywords" content={keywords} />}
-			{author && <meta name="author" content={author} />}
-			<meta name="viewport" content="width=device-width, initial-scale=1" />
-			<meta charSet="UTF-8" />
-			<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-		</Head>
-	);
-};
+const SEO = ({ title, description, keywords, author }) => (
+  <Head>
+    <title>{title}</title>
+    <meta name="description" content={description} />
+    <meta name="keywords" content={keywords} />
+    <meta name="author" content={author} />
+  </Head>
+);
 
 export default SEO;
