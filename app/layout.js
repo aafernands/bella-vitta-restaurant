416769286d2e25411668371e7d90a5fa8223e505
Alex Fernandes <62030201+aafernands@users.noreply.github.com>
2025-01-27 20:59:10 -0500

    bug fix SEO5

--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,24 @@
 // app/layout.js
-"use client"
+"use client";
 
-import React from 'react';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import { Container } from '@mui/system';
-import theme from '../themes'; // Import your custom theme if needed
-import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
-
+import React from "react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { Container } from "@mui/system";
+import theme from "../themes"; // Import your custom theme if needed
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <NavBar />
-          <Container>{children}</Container>
-          <Footer />
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<NavBar />
+					<Container>{children}</Container>
+					<Footer />
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
